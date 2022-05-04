5e6e3c6e536af0931f93938130b890ac4998b819
Leonid <orlan1211@yahoo.com>
2022-05-04 13:40:20 +0300

    Футер v.2.1.1

--- ksk-armada-app/src/components/Footer.jsx
+++ ksk-armada-app/src/components/Footer.jsx
@@ -5,8 +5,6 @@
  *  Footer component v2.1.1
  */
 
-//To-do's Доделать вёрстку соц. сетей и сделать карту, добавить нижнюю роспись, подготовить юнит тесты
-
 import React from 'react';
 import styled from 'styled-components';
 import { Button, Container, CardGroup, Card } from 'react-bootstrap';
@@ -16,7 +14,7 @@ import SmartPhone from '../Img/SmartPhone.png';
 import Chat from '../Img/Chat.png';
 import Vk from '../Img/VkWhite.png';
 import Tel from '../Img/TelWhite.png';
-
+import { MapCard } from './Ifraim/Iframe';
 
 
 const Styles = styled.div`
@@ -25,6 +23,7 @@ const Styles = styled.div`
         height: auto;
         color: #FFFFFF;
         font-family: inherit;
+        padding: 30px;
     }
     .CardSetting{
         background: transparent;
@@ -46,8 +45,51 @@ const Styles = styled.div`
         width: 40px;
     }
     .Rider{
-        width: 25rem;
+        width: 100%;
         margin: auto;
+    }
+
+    .linkIco{
+        margin: auto;
+        opacity: 60%;
+        transition: 0.5s;
+        width: 100px;
+        height: 75px;
+        padding: 5px;
+    }
+    .linkIco:hover{
+        opacity: 100%;
+    }
+    .linkIco:focus{
+        opacity: 100%;
+        background-color: gray;
+        box-shadow: 0 -4px 16px #2f3640;
+    }
+    .linkIco:active{
+        opacity: 100%;
+        background-color: black;
+        box-shadow: 0 -4px 16px #2f3640;
+    }
+    
+    .vkontacte{
+        width: 70px;
+    }
+    .telegram{
+        width: 60px;
+    }
+    .mapContact{
+        width: 100%;
+        height: 100%;
+        border-radius: 50px;
+    }
+    .SubFooter{
+        background-color: #656872;
+    }
+    .footer-copir-txt{
+        /* extrasmall/regular */
+        color: #000;
+        line-height: 150%;
+        margin-top: 20px;
     }
 `;
 
@@ -71,11 +113,11 @@ const Footer = () => {
                                     'textAlign':'center'
                                     }}
                             >
-                                <h2>
+                                <h1>
                                     Наши контакты
-                                </h2>
+                                </h1>
                             </div>
-                            <table style = {{'margin':'20px'}}>
+                            <table>
                                 <tr >
                                     <td>
                                         <img 
@@ -116,36 +158,58 @@ const Footer = () => {
                                     </td>
                                 </tr>
                             </table>
-                            <table style = {{'margin':'20px'}}>
-                                <tr>
-                                    <td>
-                                        <Button variant="link">
-                                            <img 
-                                                src = {Vk} 
-                                                alt = "VKontacte" 
-                                            />
-                                        </Button>
-                                    </td>
-                                    <td>
-                                        <Button variant="link">
-                                            <img 
-                                                src = {Tel} 
-                                                alt = "Telegram" 
-                                            />
-                                        </Button>
-                                    </td>
-                                </tr>
-                            </table>
+                            <hr />
+                            <div style={{'textAlign':'center'}}>
+                                <h1>Мы в соцсетях</h1>
+                                <Button 
+                                    variant   = 'link'
+                                    target    = '_blank'
+                                    href      = 'https://vk.com/ksk_armada'
+                                    className = 'linkIco'
+                                >
+                                    <img 
+                                        src       = {Vk} 
+                                        alt       = 'VKontacte'
+                                        className = 'vkontacte'
+                                    />
+                                </Button>
+                                <Button 
+                                    variant   = 'link'
+                                    target    = '_blank'
+                                    href      = 'https://t.me/ksk_armada'
+                                    className = 'linkIco'
+                                >
+                                    <img 
+                                        src       = {Tel}     
+                                        alt       = 'Telegram'
+                                        className = 'telegram' 
+                                    />
+                                </Button>
+                            </div>
+                                        
                         </Card>
                         <Card className = 'CardSetting'>
                             <img 
-                                src = {Rider} 
-                                alt = {Obj.alt} 
+                                src       = {Rider} 
+                                alt       = {Obj.alt} 
                                 className = 'Rider'
                             />
                         </Card>
                     </CardGroup>
                 </Container>
+                <Card 
+                    className = 'CardSetting' 
+                    style     = {{'height':'550px'}}
+                >
+                        <MapCard/>
+                </Card>
+            </div>
+            <div className = 'SubFooter'>
+                <CardGroup style = {{'textAlign':'center'}}>
+                    <Card className = 'CardSetting'>
+                        <p className = 'footer-copir-txt'>&copy;&ensp; All rights reserved. Made with by Orlan1211@yahoo.com. E.S.C. ARMADA</p>
+                    </Card>
+                </CardGroup>
             </div>
         </Styles>
         </>
