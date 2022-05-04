3bc6c29a3d941ad4258331cbeb542a08252d332d
Leonid <orlan1211@yahoo.com>
2022-05-04 18:13:04 +0300

    фото карусель

--- ksk-armada-app/src/components/HomeComp/Carusel.jsx
+++ ksk-armada-app/src/components/HomeComp/Carusel.jsx
@@ -1,9 +1,38 @@
+/**
+ * Create by Kuznetsov Leonid 
+ * 04.05.2021
+ */
+
 import React from 'react';
+import { 
+    JumboPrise,  
+    JumboEducationHorseRide, 
+    JumboHorseRide, 
+    JumboHoldUpWithHorse, 
+    JumboPhotoWithHorse 
+    } from './Jumbotrone';
+import { Carousel } from 'react-bootstrap';
 
 const Carusel = () => {
     return(
         <>
-    
+            <Carousel fade>
+                <Carousel.Item>
+                    <JumboPrise/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <JumboEducationHorseRide/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <JumboHorseRide/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <JumboHoldUpWithHorse/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <JumboPhotoWithHorse/>
+                </Carousel.Item>
+            </Carousel>
         </>
     );
 }
