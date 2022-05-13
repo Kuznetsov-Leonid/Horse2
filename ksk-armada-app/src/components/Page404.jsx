4c48ee0a1d8bae927413694ba5df12dd8a2a1cc0
Leonid <orlan1211@yahoo.com>
2022-05-13 22:44:30 +0300

    Files

---
+++ ksk-armada-app/src/components/Page404.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components';
+import { Button, Container, CardGroup, Card } from 'react-bootstrap';
+import Cowboy from '../Img/cowboy.png';
+
+const Styles = styled.div`
+
+`;
+
+const Page404 = () =>{
+    return(
+        <>
+            <div className = 'Page404'>
+
+            </div>
+        </>
+    );
+}
+
+export default Page404
